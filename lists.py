c60d60477071b21b01c66033c35e8c87dcdf17d1
Tehtehteh <tehfvpr@gmail.com>
2016-10-06 14:25:04 +0300

    Lists task

---
+++ lists.py
@@ -0,0 +1,27 @@
+#Your optional code here
+#You can import some modules or create additional functions
+
+from collections import Counter
+
+def checkio(data):
+    res = Counter(data)
+    for x in res:
+        if res[x]==1:
+            data.remove(x)
+    return data
+
+#Some hints
+#You can use list.count(element) method for counting.
+#Create new list with non-unique elements
+#Loop over original list
+
+
+if __name__ == "__main__":
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # print(checkio([1,2,3,1,3]))
+    # print(checkio([1,2,3,4,5]))
+    assert isinstance(checkio([1]), list), "The result must be a list"
+    assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3], "1st example"
+    assert checkio([1, 2, 3, 4, 5]) == [], "2nd example"
+    assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5], "3rd example"
+    assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9], "4th example"
