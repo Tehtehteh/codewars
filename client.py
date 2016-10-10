63a7d033c81ca3d433c2464dd359b27137474360
Tehtehteh <tehfvpr@gmail.com>
2016-10-10 16:45:12 +0300

    Client chat app.

--- client.py
+++ client.py
@@ -1,33 +1,20 @@
-# import socket
-#
-#
-# client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# client.connect(('255.255.255.255', 8889))
-# client.sendall(b'Fuck this!')
-# while 1:
-#     pass
+import socket
+import threading
 
-from __future__ import print_function
-from tornado.ioloop import IOLoop
-from tornado import gen
-from tornado.tcpclient import TCPClient
-from tornado.options import options, define
-
-define("host", default="localhost", help="TCP server host")
-define("port", default=8888, help="TCP port to connect to")
-define("message", default="ping", help="Message to send")
+def updates(s):
+    while 1:
+        dataBack = s.recv(1024)
+        if dataBack:
+            print('%s:' % (dataBack,))
 
 
-@gen.coroutine
-def send_message():
-    stream = yield TCPClient().connect(options.host, options.port)
-    yield stream.write((options.message + "\n").encode())
-    print("Sent to server:", options.message)
-    reply = yield stream.read_until(b"\n")
-    print("Response from server:", reply.decode().strip())
+def main():
+    s = socket.socket()
+    s.connect(('192.168.0.23', 8888))
+    while 1:
+        data = input()
+        s.send(bytes(data, encoding='utf-8'))
+        threading._start_new_thread(updates, (s,))
 
-
-if __name__ == "__main__":
-    options.parse_command_line()
-    IOLoop.current().run_sync(send_message)
+if __name__=='__main__':
+    main()
